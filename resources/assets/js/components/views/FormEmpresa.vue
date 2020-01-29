e9eded88ab3e996f9cb6b6ec7fc62d9939c34a21
Math002 <matheus002_@hotmail.com>
2020-01-29 10:52:14 -0300

    atualização das telas

---
+++ resources/assets/js/components/views/FormEmpresa.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="form-temp">
+    <h4>Empresa</h4>
+    <hr>
+      <form @submit.prevent="cadastrarEmpresa">
+        <div class="form-group">
+          <label for="nome"><strong>Nome: </strong></label>
+          <input type="text" class="form-control" id="nome" v-model="cadEmpresa.nome" placeholder="Digite o nome do responsável pela empresa">
+        </div>
+
+        <div class="form-group">
+          <label for="CNPJ"><strong>CNPJ: </strong></label>
+          <input type="text" class="form-control" id="CNPJ" v-model="cadEmpresa.cnpj" placeholder="Digite o CNPJ da empresa">
+        </div>
+
+        <div class="form-group">
+          <label for="empNome"><strong>Nome Empresarial: </strong></label>
+          <input type="text" class="form-control" id="empNome" v-model="cadEmpresa.NomeEmp" placeholder="Digite o nome fantasia da empresa">
+        </div>
+
+        <div class="form-group">
+          <label for="natJuridica"><strong>Natureza Jurídica(Grupo): </strong></label>
+          <select class="form-control" id="natJuridica" v-model="cadEmpresa.naturezaJuridica">
+            <option disabled value="">Selecione uma opção </option>
+            <option>PESSOAS FÍSICAS</option>
+            <option>ADMINISTRAÇÃO PÚBLICA</option>
+            <option>ENTIDADES EMPRESARIAIS</option>
+            <option>ENTIDADE SEM FINS LUCRATIVOS</option>
+            <option>ORGANIZAÇÕES INTERNACIONAIS</option>            
+          </select>
+        </div>
+
+         <div class="form-group">
+          <label for="gestao"><strong>Gestão: </strong></label>
+          <select class="form-control" id="gestao" v-model="cadEmpresa.gestao">
+            <option disabled value="">Selecione uma opção </option>
+            <option>PRIVADA</option>
+            <option>MUNICIPAL</option>
+            <option>ESTADUAL</option>
+            <option>DUPLA</option>           
+          </select>
+        </div>
+        <hr />
+          <div class="row">          
+            <div class="col-md-9"></div>
+            <div class="col-md-3">
+               <button type="button" class="btn btn-default mr-1">Cancelar</button>
+               <button type="submit" class="btn btn-success"><i aria-hidden="true" class="fa fa-floppy-o"></i>  <b>Salvar</b> </button>
+            </div>
+          </div>
+        
+      </form>    
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      cadEmpresa: {
+        nome: '',
+        cnpj: '',
+        NomeEmp: '',
+        naturezaJuridica: '',
+        gestao: ''
+      }
+    }
+  },
+  methods: {
+    cadastrarEmpresa() {
+      this.$store.state.Empresa = this.cadEmpresa
+      console.log( this.$store.state.Empresa)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .form-temp {
+   padding: 20px;
+   background-color: rgb(250, 250, 250);
+   border-radius: 5px;
+  }
+
+  h4 {
+    color: rgb(112, 112, 112);
+  }
+</style>
--- resources/assets/js/routes.js
+++ resources/assets/js/routes.js
@@ -1,7 +1,7 @@
 
-import Filial from "./components/views/CadastroFilial"
-import CadastroEmpresa from "./components/views/CadastroEmpresa"
-import Usuarios from "./components/views/CadastroUsuarios"
+import Filial from "./components/views/FormFilial"
+import CadastroEmpresa from "./components/views/FormEmpresa"
+import Usuarios from "./components/views/FormUsuarios"
 
 
 const routes = [
