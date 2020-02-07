dedd4c73ce8bcf081a0026242b4748668948d094
Math002 <matheus002_@hotmail.com>
2020-02-07 11:32:55 -0300

    adicionando metodos para checkbox

--- resources/assets/js/components/views/views_covite/newInvitePermissions.vue
+++ resources/assets/js/components/views/views_covite/newInvitePermissions.vue
@@ -3,7 +3,7 @@
     <div class="form-temp">
       <div class="row">
         <div class="docker">
-          <i class="fa fa-user-circle-o fa-4x" aria-hidden="true"></i>
+         <i class="fa fa-user fa-4x" aria-hidden="true"></i>
         
         
           <label>{{ user }}</label>
@@ -15,7 +15,7 @@
     </div>
 
     <div class="form-temp">
-      <div class="container container-new">
+      <div class="container ">
         <h4>Quem terá acesso a esse usuário</h4>
         <hr />
         <div>
@@ -37,10 +37,10 @@
       <br />
       <div class="container">
         <div class="parent">         
-            <input class="magic-checkbox" type="checkbox" id="Financas" value="Financas" />
+            <input class="magic-checkbox" type="checkbox" id="check" value="Financas" />
             <label for="Financas">Financeiro</label>       
-            <a class="btn btn-default" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-               <i class="fa fa-chevron-down" aria-hidden="true"></i>
+            <a class="btn btn-default" @click="iconF = !iconF" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+               <i :class="[iconF ? 'fa-chevron-up' : 'fa-chevron-down', 'fa']" />
             </a>          
         </div>
         <br />
@@ -48,42 +48,42 @@
           <div class="container vl">
             <div class="container">
               <div class="row">
-                <input class="magic-checkbox" type="checkbox" id="teste" value="teste" />
-                <label for="teste">Teste11</label>
+                <input class="magic-checkbox" type="checkbox" id="teste" value="Relatórios Financeiros" @click="checkAll" v-model="isCheckAll"/>
+                <label for="teste"> Relatórios Financeiros</label>
               </div>
               <div class="container">
                 <div>
-                  <input class="magic-checkbox" type="checkbox" id="Teste1" value="teste1" />
-                  <label for="teste1">Teste1</label>
+                  <input class="magic-checkbox" type="checkbox" id="op1" value="op1"  v-model="permissoes" @change="updateCheckall"/>
+                  <label for="op1">Cadastrar relatórios financeiros</label>
                 </div>
                 <div>
-                  <input class="magic-checkbox" type="checkbox" id="Teste2" value="Teste2" />
-                  <label for="Teste2">Teste2</label>
+                  <input class="magic-checkbox" type="checkbox" id="op2" value="op2" v-model="permissoes" @change="updateCheckall" />
+                  <label for="op2">Editar relatórios financeiros</label>
                 </div>
                 <div>
-                  <input class="magic-checkbox" type="checkbox" id="Teste3" value="Teste3" />
-                  <label for="Teste3">Teste3</label>
+                  <input class="magic-checkbox" type="checkbox" id="op3" value="op3" v-model="permissoes" @change="updateCheckall"/>
+                  <label for="op3">Deletar relatórios financeiros</label>
                 </div>
               </div>
               <br />
               <div class="row">
-                <input class="magic-checkbox" type="checkbox" id="teste21" value="teste21" />
-                <label for="teste21">Teste21</label>
+                <input class="magic-checkbox" type="checkbox" id="teste21" value="Pagamentos"  @click="checkAll"/>
+                <label for="teste21">Pagamentos</label>
               </div>
-              <div class="container">
-                <div>
-                  <input class="magic-checkbox" type="checkbox" id="Teste1" value="teste1" />
-                  <label for="teste1">Teste1</label>
+                <div class="container">
+                  <div>
+                    <input class="magic-checkbox" type="checkbox" id="Teste1" value="teste1" />
+                    <label for="teste1">Visualizar Pagamentos</label>
+                  </div>
+                  <div>
+                    <input class="magic-checkbox" type="checkbox" id="Teste2" value="Teste2" />
+                    <label for="Teste2">Agendar Pagamentos</label>
+                  </div>
+                  <div>
+                    <input class="magic-checkbox" type="checkbox" id="Teste3" value="Teste3" />
+                    <label for="Teste3">Gerar relaórios de pagamentos</label>
+                  </div>
                 </div>
-                <div>
-                  <input class="magic-checkbox" type="checkbox" id="Teste2" value="Teste2" />
-                  <label for="Teste2">Teste2</label>
-                </div>
-                <div>
-                  <input class="magic-checkbox" type="checkbox" id="Teste3" value="Teste3" />
-                  <label for="Teste3">Teste3</label>
-                </div>
-              </div>
             </div>
           </div>
         </div>
@@ -93,8 +93,8 @@
         <div class="parent">         
             <input class="magic-checkbox" type="checkbox" id="Agendamentos" value="Agendamentos" />
             <label for="Agendamentos">Agendamentos</label>       
-            <a class="btn btn-default" type="button" data-toggle="collapse" data-target="#collapseAgendamentos" aria-expanded="false" aria-controls="collapseAgendamentos">
-               <i class="fa fa-chevron-down" aria-hidden="true"></i>
+            <a class="btn btn-default" type="button" @click="iconE = !iconE" data-toggle="collapse" data-target="#collapseAgendamentos" aria-expanded="false" aria-controls="collapseAgendamentos">
+               <i :class="[iconE ? 'fa-chevron-up' : 'fa-chevron-down', 'fa']" />
             </a>          
         </div>
         <br />
@@ -105,20 +105,20 @@
                 <input class="magic-checkbox" type="checkbox" id="teste" value="teste" />
                 <label for="teste">Teste11</label>
               </div>
-              <div class="container">
-                <div>
-                  <input class="magic-checkbox" type="checkbox" id="Teste1" value="teste1" />
-                  <label for="teste1">Teste1</label>
+                <div class="container">
+                  <div>
+                    <input class="magic-checkbox" type="checkbox" id="Teste1" value="teste1" />
+                    <label for="teste1">Teste1</label>
+                  </div>
+                  <div>
+                    <input class="magic-checkbox" type="checkbox" id="Teste2" value="Teste2" />
+                    <label for="Teste2">Teste2</label>
+                  </div>
+                  <div>
+                    <input class="magic-checkbox" type="checkbox" id="Teste3" value="Teste3" />
+                    <label for="Teste3">Teste3</label>
+                  </div>
                 </div>
-                <div>
-                  <input class="magic-checkbox" type="checkbox" id="Teste2" value="Teste2" />
-                  <label for="Teste2">Teste2</label>
-                </div>
-                <div>
-                  <input class="magic-checkbox" type="checkbox" id="Teste3" value="Teste3" />
-                  <label for="Teste3">Teste3</label>
-                </div>
-              </div>
               <br />
               <div class="row">
                 <input class="magic-checkbox" type="checkbox" id="teste21" value="teste21" />
@@ -191,8 +191,8 @@
 
        <div class="container">
         <div class="parent">         
-            <input class="magic-checkbox" type="checkbox" id="Agendamentos" value="Agendamentos" />
-            <label for="Agendamentos">Pacientes</label>       
+            <input class="magic-checkbox" type="checkbox" id="Pacientes" value="Pacientes" />
+            <label for="Pacientes">Pacientes</label>       
             <!-- <a class="btn btn-default" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
                <i class="fa fa-chevron-down" aria-hidden="true"></i>
             </a>           -->
@@ -215,7 +215,7 @@
         <div class="col-md-8"></div>
         <div class="col-md-4">
           <button class="btn btn-deafult mr-1">Voltar</button>
-          <button class="btn btn-primary">Continuar</button>
+          <button class="btn btn-primary" @click="getPermissoes">Continuar</button>
         </div>
       </div>
     </div>
@@ -226,10 +226,53 @@
 export default {
   props: ["user"],
   data() {
-    return { teste: false };
+    return { 
+      iconF: false,
+      iconE: false,
+      isCheckAll: false,
+      permissoes: []
+       
+    };
   },
-  methods: {}
-};
+  methods: {
+    getPermissoes() {
+      console.log(this.permissoes);
+    },
+    checkAll(e) {
+
+      //console.log(e.target.value);      
+      // this.isCheckAll = !this.isCheckAll;
+      // this.permissoes = [];
+      // if (this.isCheckAll) {
+      //   this.permissoes = ["op1", "op2", "op3"]
+      // }
+
+      switch (e.target.value) {
+        case 'Relatórios Financeiros' :
+          //console.log("Relatórios Financeiros");
+          this.isCheckAll = !this.isCheckAll;
+          this.permissoes = [];
+          if (this.isCheckAll) {
+            this.permissoes = ["op1", "op2", "op3"]
+          }
+          break;
+        case 'Pagamentos' :
+          console.log('Pagamentos');
+          break;
+        default :
+          console.log('Nenhuma opção');
+      }
+    },
+    updateCheckall() {
+      //Método para verificar todas as chechboxs
+      if (this.permissoes  == 3) {
+        this.isCheckAll = true;
+      } else {
+        this.isCheckAll = false;
+      }
+    }  
+   }
+}
 </script>
 
 <style>
