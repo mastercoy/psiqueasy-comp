70039a0593e2fe62ef6436d23563a51ebe4e71d3
Math002 <matheus002_@hotmail.com>
2020-02-27 14:06:28 -0300

    atualização de perfis

--- resources/assets/js/components/pages/Usuarios/BotoesTestes.vue
+++ resources/assets/js/components/pages/Usuarios/BotoesTestes.vue
@@ -3,31 +3,31 @@
     <h2>Página para testes de aplicação de botões</h2><hr>
     <div class="container">
       <div class="row">
-        <button class="btn btn-primary btn-block" :disabled="!checkPermissoes(1)" @click="testFunc">
+        <button class="btn btn-primary btn-block" :disabled="checkPermissoes(1)" @click="testFunc">
           Editar Empresa
         </button>
-        <button class="btn btn-warning btn-block" :disabled="!checkPermissoes(2)" @click="testFunc">
+        <button class="btn btn-warning btn-block" :disabled="checkPermissoes(20)" @click="testFunc">
           Adicionar Filial
         </button>
-        <button class="btn btn-secondary btn-block" :disabled="!checkPermissoes(10)" @click="testFunc">
+        <button class="btn btn-secondary btn-block" :disabled="checkPermissoes(3)" @click="testFunc">
           Editar Filial
         </button>
-        <button class="btn btn-danger btn-block" :disabled="!checkPermissoes(10)" @click="testFunc">
+        <button class="btn btn-danger btn-block" :disabled="checkPermissoes(4)" @click="testFunc">
           Deletar Filial
         </button>
-        <button class="btn btn-primary btn-block" :disabled="!checkPermissoes(4)" @click="testFunc">
+        <button class="btn btn-primary btn-block" :disabled="checkPermissoes(5)" @click="testFunc">
           Adicionar User
         </button>
-        <button class="btn btn-warning btn-block" :disabled="!checkPermissoes(1)" @click="testFunc">
+        <button class="btn btn-warning btn-block" :disabled="checkPermissoes(6)" @click="testFunc">
           Editar User
         </button>
-        <button class="btn btn-danger btn-block" :disabled="!checkPermissoes(1)" @click="testFunc">
+        <button class="btn btn-danger btn-block" :disabled="checkPermissoes(7)" @click="testFunc">
           Deletar User
         </button>
-        <button class="btn btn-secondary btn-block" :disabled="!checkPermissoes(1)" @click="testFunc">
+        <button class="btn btn-secondary btn-block" :disabled="checkPermissoes(8)" @click="testFunc">
           Editar Perfil
         </button>
-        <button class="btn btn-secondary btn-block" :disabled="!checkPermissoes(8)" @click="testFunc">
+        <button class="btn btn-secondary btn-block" :disabled="checkPermissoes(9)" @click="testFunc">
           Editar Perfil
         </button>
       </div>
@@ -38,9 +38,15 @@
 
 <script>
 export default {
+  mounted() {
+     axios.get(`/api/permissoes-perfil-json/3`)
+          .then(({data}) => {
+            console.log(data);
+          }); 
+  },
   data() {
     return {
-      permissoes: [1, 10, 30]
+      permissoes: []
     }
   },
   methods: {
@@ -48,6 +54,7 @@ export default {
       console.log("Está ativo!");
     },
      checkPermissoes(e) { //Função que retorna se o 
+     console.log(e);
           if( this.permissoes.includes(e)){
             return true;
           }else {
